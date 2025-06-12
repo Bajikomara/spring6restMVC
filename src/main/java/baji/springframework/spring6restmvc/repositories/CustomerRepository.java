d57dfab199e7fa7be69fc7bf5f073c693865ed2e
Bajikomara <kbaji52@gmail.com>
2025-06-13 00:40:01 +0530

    init commit

---
+++ src/main/java/baji/springframework/spring6restmvc/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package baji.springframework.spring6restmvc.repositories;
+
+import baji.springframework.spring6restmvc.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
