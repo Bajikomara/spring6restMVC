20c3e38715ccea47754392869bb00c82b128ac62
Bajikomara <kbaji52@gmail.com>
2025-06-12 16:07:01 +0530

    init commit

--- src/test/java/baji/springframework/spring6restmvc/Controller/BeerControllerTest.java
+++ src/test/java/baji/springframework/spring6restmvc/Controller/BeerControllerTest.java
@@ -1,5 +1,8 @@
 package baji.springframework.spring6restmvc.Controller;
 
+import baji.springframework.spring6restmvc.model.Beer;
+import baji.springframework.spring6restmvc.services.BeerService;
+import baji.springframework.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,34 +11,40 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //@SpringBootTest
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
-//    @Autowired
-//    BeerController beerController;
-
     @Autowired
     MockMvc mockMvc;
 
     @MockitoBean
-    BeerController controller;
+    BeerService beerService;
 
+    BeerServiceImpl beerServiceimpl = new BeerServiceImpl();
 
     @Test
     void getBeerById() throws Exception {
+
+        Beer testBeer = beerServiceimpl.listBeers().get(0);
+        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-//        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
     }
 
 }
