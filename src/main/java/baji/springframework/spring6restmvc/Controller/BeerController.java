4abda91c8e0e275059c05c3617e1dabda9d07aa8
Bajikomara <kbaji52@gmail.com>
2025-06-12 13:16:37 +0530

    init commit

--- src/main/java/baji/springframework/spring6restmvc/Controller/BeerController.java
+++ src/main/java/baji/springframework/spring6restmvc/Controller/BeerController.java
@@ -20,6 +20,14 @@ import java.util.UUID;
 public class BeerController {
     private final BeerService beerService;
 
+    @PutMapping("{beerId}")
+    public ResponseEntity updateById(@PathVariable("beerId")UUID beerId, @RequestBody Beer beer){
+
+        beerService.updateBeerById(beerId, beer);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
     @PostMapping
 //    @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity handlepost(@RequestBody Beer beer) {
