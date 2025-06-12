572475625fdba628f1b74af5c8201bd8d531e82a
Bajikomara <kbaji52@gmail.com>
2025-06-12 20:20:43 +0530

    init commit

--- src/test/java/baji/springframework/spring6restmvc/Controller/CustomerControllerTest.java
+++ src/test/java/baji/springframework/spring6restmvc/Controller/CustomerControllerTest.java
@@ -3,6 +3,8 @@ package baji.springframework.spring6restmvc.Controller;
 import baji.springframework.spring6restmvc.model.Customer;
 import baji.springframework.spring6restmvc.services.CustomerService;
 import baji.springframework.spring6restmvc.services.CustomerServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,23 +13,48 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.core.Is.is;
 
 @WebMvcTest(CustomerController.class)
 class CustomerControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @MockitoBean
     CustomerService customerService;
 
-    @Autowired
-    MockMvc mockMvc;
+    CustomerServiceImpl customerServiceImpl;
 
-    CustomerServiceImpl customerServiceImpl= new CustomerServiceImpl();
+    @BeforeEach
+    void setUp() {
+        customerServiceImpl = new CustomerServiceImpl();
+    }
+
+    @Test
+    void testCreateNewCustomer() throws Exception {
+        Customer customer = customerServiceImpl.listCustomers().get(0);
+        customer.setVersion(null);
+        customer.setId(null);
+
+        given(customerService.saveNewCustomer(any(Customer.class))).willReturn(customerServiceImpl.listCustomers().get(1));
+        mockMvc.perform(post("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+    }
+
 
     @Test
     void testListCustomers() throws Exception {
