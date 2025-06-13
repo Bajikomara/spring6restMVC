6cf1d646688621c920ae164449b6beb6aa9f958f
Bajikomara <kbaji52@gmail.com>
2025-06-13 11:27:09 +0530

    init commit

---
+++ src/main/java/baji/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,48 @@
+package baji.springframework.spring6restmvc.services;
+
+import baji.springframework.spring6restmvc.mappers.BeerMapper;
+import baji.springframework.spring6restmvc.model.BeerDTO;
+import baji.springframework.spring6restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return List.of();
+    }
+
+    @Override
+    public BeerDTO getBeerById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void deleteBeerById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+}
