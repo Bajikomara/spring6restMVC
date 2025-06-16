05474214e98ac432b5a9fb66f14818fbdbbca2ba
Bajikomara <kbaji52@gmail.com>
2025-06-16 22:13:39 +0530

    init commit

--- src/test/java/baji/springframework/spring6restmvc/Controller/BeerControllerIT.java
+++ src/test/java/baji/springframework/spring6restmvc/Controller/BeerControllerIT.java
@@ -34,6 +34,23 @@ class BeerControllerIT {
     @Autowired
     BeerMapper beerMapper;
 
+    @Rollback
+    @Transactional
+    @Test
+    void deletebyIdFound() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        ResponseEntity responseEntity = beercontroller.deleteById(beer.getId());
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+        assertThat(beerRepository.findById(beer.getId()).isEmpty());
+
+//        Beer foundBeer = beerRepository.findById(beer.getId()).get();
+//        assertThat(foundBeer).isNull();
+    }
+
+    @Rollback
+    @Transactional
     @Test
     void testUpdateNotFound() {
         assertThrows(NotFoundException.class, () -> {
