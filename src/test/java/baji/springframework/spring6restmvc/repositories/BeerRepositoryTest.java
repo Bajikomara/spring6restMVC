b6d8df9421f8342b35d385aa92ee7ad613fb2164
Bajikomara <kbaji52@gmail.com>
2025-06-17 01:19:32 +0530

    init commit

--- src/test/java/baji/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/baji/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package baji.springframework.spring6restmvc.repositories;
 
 import baji.springframework.spring6restmvc.entities.Beer;
+import baji.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,7 +20,12 @@ class BeerRepositoryTest {
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("MY BEER")
+                        .beerStyle(BeerStyle.ALE)
+                        .upc("784521")
+                        .price(new BigDecimal("11.99"))
                 .build());
+
+        beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
     }
