002ca7f586701a3e02b76b3de80819de3e78b0eb
Bajikomara <kbaji52@gmail.com>
2025-06-18 22:43:06 +0530

    init commit

--- src/main/java/baji/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/baji/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -3,7 +3,10 @@ package baji.springframework.spring6restmvc.repositories;
 import baji.springframework.spring6restmvc.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
