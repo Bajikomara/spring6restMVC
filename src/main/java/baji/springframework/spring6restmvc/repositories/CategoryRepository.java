a2f0ac5a969de986412c0288855b3606a050b099
Bajikomara <kbaji52@gmail.com>
2025-06-21 00:00:36 +0530

    init commit

---
+++ src/main/java/baji/springframework/spring6restmvc/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package baji.springframework.spring6restmvc.repositories;
+
+import baji.springframework.spring6restmvc.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
