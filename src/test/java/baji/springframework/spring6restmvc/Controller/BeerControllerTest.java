f397551291fd803335193818e5cb06decf5a3f58
Bajikomara <kbaji52@gmail.com>
2025-06-22 19:42:18 +0530

    Added OAuth2 to patch beer test

--- src/test/java/baji/springframework/spring6restmvc/Controller/BeerControllerTest.java
+++ src/test/java/baji/springframework/spring6restmvc/Controller/BeerControllerTest.java
@@ -22,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -31,6 +32,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -89,7 +91,14 @@ class BeerControllerTest {
         beerMap.put("beerName", "New Name");
 
         mockMvc.perform(patch("/api/v1/beer/" + beer.getId())
-                        .with(httpBasic(USERNAME, PASSWORD))
+                        .with(jwt().jwt(jwt ->{
+                            jwt.claims(claims -> {
+                                claims.put("scope", "message-read");
+                                claims.put("scope", "message-write");
+                            })
+                                    .subject("messaging-client")
+                                    .notBefore(Instant.now().minusSeconds(5l));
+                        }))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(beerMap)))
